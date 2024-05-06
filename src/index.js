84ad7544ee6c54bfcac49216ed21eaecd9a29237
vinayki <kharevinayki7@gmail.com>
2024-05-06 23:32:27 +0530

    using partials

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 var express = require('express')
 var path = require('path')
 var app = express()
+const hbs = require("hbs")
 
 const staticPath = path.join(__dirname, '../public');
+const templatePath = path.join(__dirname, '../template');
+const partialsPath = path.join(__dirname, '../partials');
+
+
 
 app.set("view engine","hbs");
+app.set("views",templatePath);
+hbs.registerPartials()
 
 app.get('/', function (req, res) {
   res.render()
